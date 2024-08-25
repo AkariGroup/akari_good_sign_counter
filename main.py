484f8a4bd259a153b9cd61ebb4f5075d6bf52de7
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-08-25 22:46:07 +0900

    Fix error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import grpc
 import json
 import threading
 import sys
@@ -19,6 +20,12 @@ import motion_server_pb2_grpc
 
 json_path = "log/log.json"
 
+motion_server_port = "localhost:50055"
+channel = grpc.insecure_channel(motion_server_port)
+stub = motion_server_pb2_grpc.MotionServerServiceStub(channel)
+akari = AkariClient()
+m5 = akari.m5stack
+
 
 def display_good_count(num) -> None:
     """
@@ -28,35 +35,32 @@ def display_good_count(num) -> None:
         num: int 表示するいいねの数
 
     """
-    with AkariClient() as akari:
-        m5 = akari.m5stack
-        m5.set_display_text(
-            text=str(num),
-            pos_x=Positions.CENTER,
-            pos_y=Positions.CENTER,
-            size=8,
-            text_color=Colors.RED,
-            back_color=Colors.WHITE,
-            refresh=True,
-        )
-        m5.set_display_text(
-            text="いいね！",
-            pos_x=Positions.BOTTOM,
-            pos_y=Positions.RIGHT,
-            size=3,
-            text_color=Colors.BLACK,
-            back_color=Colors.WHITE,
-            refresh=True,
-        )
+    m5.set_display_text(
+        text=str(num),
+        pos_x=Positions.CENTER,
+        pos_y=Positions.CENTER,
+        size=12,
+        text_color=Colors.RED,
+        back_color=Colors.WHITE,
+        refresh=True,
+    )
+    m5.set_display_text(
+        text="いいね!",
+        pos_x=Positions.BOTTOM,
+        pos_y=Positions.RIGHT,
+        size=5,
+        text_color=Colors.BLACK,
+        back_color=Colors.WHITE,
+        refresh=False,
+    )
 
 
-def good_update(num: int, stub: Any) -> None:
+def good_update(num: int) -> None:
     """
     いいねの数を更新する
 
     Args:
         num: int いいねの数
-        stub: Any gRPCのstub
     """
     motion = "nod"
     print("Send motion " + str(motion))
@@ -67,19 +71,18 @@ def good_update(num: int, stub: Any) -> None:
             )
         )
     except Exception as e:
-        print(e)
-    with AkariClient() as akari:
-        m5 = akari.m5stack
-        m5.set_display_text(
-            text="Thank you！",
-            pos_x=Positions.CENTER,
-            pos_y=Positions.CENTER,
-            size=5,
-            text_color=Colors.RED,
-            back_color=Colors.WHITE,
-            refresh=True,
-        )
-    time.sleep(3)
+        print("akari motion server set motion error")
+    m5.set_display_text(
+        text="Thank you!",
+        pos_x=Positions.CENTER,
+        pos_y=Positions.CENTER,
+        size=5,
+        text_color=Colors.RED,
+        back_color=Colors.WHITE,
+        refresh=True,
+        sync=True,
+    )
+    time.sleep(1)
     display_good_count(num)
     result = {"good_count": num}
     with open(json_path, mode="wt", encoding="utf-8") as f:
@@ -102,18 +105,24 @@ def main() -> None:
         trace=0,
     )
     json_open = open(json_path, "r")
-    log = json.load(json_open)
+    try:
+        log = json.load(json_open)
+    except json.JSONDecodeError:
+        log = {"good_count": 0}
     total_good_count = 0
     if log["good_count"] is not None:
         total_good_count = log["good_count"]
 
     renderer = HandFaceRenderer(tracker=tracker, output=None)
-    HAND_GOOD_THRESHOLD = 20  # この回数以上OKが検出されたら1いいねとカウント
+    HAND_GOOD_THRESHOLD = 10  # この回数以上OKが検出されたら1いいねとカウント
     HAND_ERROR_THRESHOLD = (
-        10  # HAND_GOOD_THRESHOLDこの回数以上OK以外が検出されたらリセット
+        5  # HAND_GOOD_THRESHOLDこの回数以上OK以外が検出されたらリセット
     )
+    GOOD_LOCK_TIME = 2  # いいね後のロック時間
+    last_count_time = time.time()
     hand_good_count = [0, 0]  # OKのカウント回数[左手、右手]
     hand_good_error_count = [0, 0]  # OKが抜けた回数[左手、右手]
+    display_good_count(total_good_count)
     while True:
         frame, faces, hands = tracker.next_frame()
         if frame is None:
@@ -129,12 +138,16 @@ def main() -> None:
             else:
                 continue
             gesture_result = hand.gesture
-            if gesture_result == "OK":
+            if (
+                gesture_result == "OK"
+                and time.time() - last_count_time > GOOD_LOCK_TIME
+            ):
                 hand_good_count[hand_num] += 1
             else:
                 hand_good_error_count[hand_num] += 1
             # OKの検出回数が一定回数になったらいいねを加算
             if hand_good_count[hand_num] > HAND_GOOD_THRESHOLD:
+                last_count_time = time.time()
                 total_good_count += 1
                 hand_good_count[hand_num] = 0
                 hand_good_error_count[hand_num] = 0
@@ -148,7 +161,11 @@ def main() -> None:
 
         key = renderer.waitKey(delay=1)
         if key == 27 or key == ord("q"):
+            good_thread.join()
+            time.sleep(1)
+            m5.set_display_image("/jpg/logo320.jpg")
             break
+
     renderer.exit()
     tracker.exit()
 
